20f6e03e80a263eb41e250617d67d47a9622b97d
Unknown <nokid_gr@hotmail.it>
2018-05-26 10:13:20 +0200

    TypeORM connected to Postgres

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 import 'reflect-metadata'
 import {createKoaServer} from "routing-controllers"
 import Controller from "./controller"
+import setupDb from './db'
+
 
 const port = process.env.PORT || 4000
 
@@ -9,4 +11,8 @@ const app = createKoaServer({
    controllers: [Controller]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
